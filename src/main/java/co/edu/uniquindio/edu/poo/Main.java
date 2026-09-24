e316024d1cb26d3e9e61c01641f546e03b539f6c
juanemedinam-ux <juane.medinam@uqvirtual.edu.co>
2026-09-24 11:35:25 -0500

    Changes by juamis07

--- src/main/java/co/edu/uniquindio/edu/poo/Main.java
+++ src/main/java/co/edu/uniquindio/edu/poo/Main.java
@@ -4,10 +4,11 @@ package co.edu.uniquindio.edu.poo;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-
+        int dato=3;
         }
     }
 
 
 //ola prueba 1 de code with me
 // helloworld(prin)
+
